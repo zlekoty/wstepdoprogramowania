b9d6540a6705d5b6a969db660bea96ebd708a3c4
zlekoty <77397198+zlekoty@users.noreply.github.com>
2021-01-28 21:55:21 +0100

    Add files via upload

--- informatka/l7/l7z4.py
+++ informatka/l7/l7z4.py
@@ -1,20 +1,23 @@
 import math
 class Rocket:
-    def __init__(self,a=0,b=0,v=0,m=0):
+    def __init__(self,a=0,b=0,v=0,s=[]):
         self.x=a
         self.y=b
-        self.m=m
         self.v=v
+        self.s=[]
     def __str__(self):
         return f"({self.x},{self.y})"
     def move(self,x,y):
         self.x=self.x+x
         self.y=self.y+y
+        z=math.sqrt(x**2+y**2)
+        self.s.append(z)
     def get_position(self):
         return (self.x, self.y)
     def get_distance(self,Rocket2):
         return 'odległość', m.sqrt((self.x-Rocket2.x)**2+(self.y-Rocket2.y)**2)
-    def promień(self):
-        return math.sqrt((self.x)**2+(self.y)**2)
-    def ziemia(self):
-        
+    def droga(self):
+        d=0
+        for i in range(len(self.s)):
+            d=d+self.s[i]
+        return d
